7c0bdbbe4f16161febb323f85bf8e07a5fbb53ff
Sandrine Ayral <sandrine.ayral@gmail.com>
2018-04-02 15:59:09 +0100

    initial code for flat.js following Seb's lecture

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Flat from './components/flat';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+        <div className="map">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import Hello from './components/hello';
+// import registerServiceWorker from './registerServiceWorker';
+
+// const root = <div>
+//   <Hello firstName="Boris" lastName="Paillard" />
+//   <Hello firstName="Romain" lastName="Paillard" />
+// </div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
